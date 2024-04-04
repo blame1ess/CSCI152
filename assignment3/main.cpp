44b956b2829d667cd57597d23bbaa785efdc11e9
Salikh <pernebek.sm@gmail.com>
2024-04-04 09:50:05 +0500

    assignment finished

---
+++ assignment3/main.cpp
@@ -0,0 +1,213 @@
+
+#include <random>
+#include <algorithm>
+
+#include "set.h"
+#include "timetable.h"
+#include "timer.h"
+
+
+std::vector< int > randomvector( size_t sz ) 
+{
+   std::vector< int > res;
+   for( size_t i = 0; i != sz; ++ i )
+   {
+      res. push_back( rand( ) ); 
+   } 
+   return res;
+}
+
+   
+int main( int argc, char* argv[ ] )
+{
+
+#if 0
+   // A few simple tests with std::string.
+   set small;    // A small test tree.
+
+   // Case-insensitive set, comparator testing
+   small.insert("X");
+   small.insert("x");
+
+   std::cout << small << "\n";
+   std::cout << small. height( ) << "\n";
+   small. insert( "if" );
+   std::cout << small. height( ) << "\n"; 
+   small. insert( "thE" ); 
+   std::cout << small. height( ) << "\n";
+   small. insert( "vaLue" );
+   std::cout << small << "\n";
+   std::cout << small. height( ) << "\n";
+   small. clear( );
+   std::cout << "this is the tree: " << small << "\n";
+   set small3 = small;
+   std::cout << small3 << "\n";
+
+   std::cout << small. contains( "Xx" ) << "\n";
+   std::cout << small. contains( "vAlUe" ) << "\n";
+#endif
+
+#if 0
+   Some more tests with std::string:
+   Make sure that your set is case-insensitive.
+   small. insert( "oskemen" );
+   std::cout << small.height() << std::endl;
+
+   std::cout << small. contains( "kostanay" ) << "\n";
+
+   small. insert( "atyrau" );
+   small. insert( "nursultan" );
+   small. insert( "Almaty" );
+   small. insert( "shymkent" ); 
+   small. insert( "almaty" ); 
+   small. insert( "pavlodar" );
+
+   small. insert( "PavloDar" ); 
+   small. insert( "sHYmKeNt" );  
+   std::cout << small << "\n";
+
+
+   small. remove( "PavLoDar" );
+   small. remove( "almaty" );
+   std::cout << small. remove( "if" );
+
+   std::cout << small << "\n";
+
+   small. checksorted( );
+   std::cout << small << "\n"; 
+   std::cout << small. size( ) << "\n";
+   If the numbers differ much, the tree is badly balanced.
+   std::cout << "Height is " << small. height( );
+   std::cout << " should be approximately " << log_base2( small. size( )) << "\n";
+
+   set small3;
+   set small4; // for assignment
+
+   small3.insert("val1");
+   small3.insert("val2");
+   std::cout << small3;
+
+   auto small2 = small3; // Copy constructor.
+   std::cout << small2 << "\n";
+   std::cout << "size = " << small2. size( ) << "\n";
+   std::cout << "height = " << small2. height( ) << "\n";
+   small4 = small3;      // Assignment.
+   std::cout << small4 << "\n";
+
+   small3 = small3;       // Self assignment.
+
+   return 0;
+
+#endif 
+
+#if 0
+   // Measurements using int.
+
+   timetable tab( std::string( "BST-based set" ));
+   for( size_t s = 1000; s < 4096 * 1000; s = 2 * s )
+   {
+      std::cout << "----------------------------------------\n";
+
+      // Create random test set of size s:
+
+      std::vector< int > values = randomvector(s);
+      std::cout << "z";
+      
+      //  Turn on if you want to see the test vector.
+
+      bool i_want_to_see_the_test_vector = false;
+      if( i_want_to_see_the_test_vector )
+      {
+         std::cout << "values: ";
+         for( const auto& s : values )
+            std::cout << s << " ";
+         std::cout << "\n";
+      }
+
+      // Uncomment to see the performance of a badly balanced tree:
+
+      bool i_want_to_see_the_bad_performance_of_an_unbalanced_tree = false; 
+      if( i_want_to_see_the_bad_performance_of_an_unbalanced_tree )
+      { 
+         std::sort( values. begin( ), values. end( ));
+      }
+      set someset1;
+
+      timer tt; 
+      size_t nr_insertions = 0;
+      size_t nr_deletions = 0;
+
+      for( size_t i = 0; i != values. size( ); ++ i ) 
+      {
+         nr_insertions += someset1. insert( values[i] );
+      }
+
+      // We do some deletions too:
+
+      for( const auto& s : values )
+      {
+         // Remove the odd values:
+
+         if( s & 1 )
+            nr_deletions += someset1. remove( s );
+
+         nr_deletions += someset1. remove( s + 2 );  // Probably not there.
+      }
+    
+      // std::cout << someset1 << "\n";
+      std::cout << nr_insertions << " " << nr_deletions << "\n";
+
+      auto someset2 = someset1;
+
+      if( nr_insertions - nr_deletions != someset2. size( )) 
+      {
+         std::cout << "nr = " << nr_insertions << "\n";
+         std::cout << "size( ) = " << someset2. size( ) << "\n"; 
+         throw std::runtime_error( 
+                       "something went wrong with counting insertions" );
+      }
+
+      // Don't use size( )! It is O(n), and will mess up your performance. 
+
+      tab. insert( s, tt. time( )); 
+
+      std::cout << "number of insertions " << nr_insertions << "\n";
+      std::cout << "number of deletions " << nr_deletions << "\n"; 
+      std::cout << "tree-height = " << someset2. height( ) << ", " << "log_base2 = " << log_base2( someset2. size( )) << "\n";
+
+      std::cout << "finished test for " << s << "\n";
+   } 
+
+   std::cout << tab << "\n"; 
+   std::cout << "totaltime " << tab. totaltime( ) << "\n";
+#endif
+
+#if 1
+    timetable tab("BST-based set");
+    for (size_t s = 1000; s <= 256000; s *= 2) {
+        set testSet;
+        timer t;
+        std::vector<int> values = randomvector(s); // For string, adjust accordingly.
+
+        // Optional: sort values for unbalanced tree measurement.
+        // std::sort(values.begin(), values.end());
+
+        for (size_t i = 0; i != values.size(); ++i) {
+            testSet.insert(values[i]);
+        }
+
+        tab.insert(s, t.time());
+        // Optionally, check and document the height vs log_base2(size) comparison here.
+    }
+    std::cout << tab << "\n";
+#endif
+}
+
+/*
+ * Performance table of BST-based set (inputsize/runtime in seconds):
+     1000/1.7300e-04     2000/3.8600e-04     4000/6.3700e-04     8000/1.2430e-03     16000/2.8940e-03     32000/6.9920e-03     64000/1.5419e-02     128000/2.5691e-02     256000/5.8683e-02
+
+ *
+ *
+ */
+
---
+++ assignment3/set.cpp
@@ -0,0 +1,274 @@
+
+#include "set.h"
+
+int case_insensitive_cmp::operator( ) ( const std::string& s1, const std::string& s2 ) const
+{
+    size_t len = std::min(s1.length(), s2.length());
+    for (size_t i = 0; i < len; ++i) {
+        char c1 = std::tolower(static_cast<unsigned char>(s1[i]));
+        char c2 = std::tolower(static_cast<unsigned char>(s2[i]));
+        if (c1 != c2) {
+            return (c1 < c2) ? -1 : 1;
+        }
+    }
+
+    if (s1.length() == s2.length()) {
+        return 0;
+    } else {
+        return (s1.length() < s2.length()) ? -1 : 1;
+    }
+}
+
+
+const treenode* find( const treenode* n, const treenode::valtype& val )
+{
+    treenode::valcmp cmp;
+    while (n != nullptr) {
+        int compare = cmp(n->val, val);
+        if (compare == 0) {
+            return n;
+        } else if (compare > 0) {
+            n = n->left;
+        } else {
+            n = n->right;
+        }
+    }
+
+    return nullptr;
+}
+
+treenode** find( treenode** n, const treenode::valtype& val )
+{
+    treenode::valcmp cmp;
+    while (*n != nullptr) {
+        int compare = cmp((*n)->val, val);
+        if (compare == 0) {
+            return n;
+        } else if (compare > 0) {
+            n = &((*n)->left);
+        } else {
+            n = &((*n)->right);
+        }
+    }
+
+    return n;
+}
+
+size_t size( const treenode* n )
+{
+    if (n == nullptr) {
+        return 0;
+    }
+
+    return 1 + size(n->left) + size(n->right);
+}
+
+size_t height( const treenode* n )
+{
+    if (n == nullptr) {
+        return 0;
+    }
+
+    size_t left_height = height(n->left);
+    size_t right_height = height(n->right);
+
+    return 1 + std::max(left_height, right_height);
+}
+
+void deallocate( treenode* n )
+{
+    if (n != nullptr) {
+        deallocate(n->left);
+        deallocate(n->right);
+        delete n;
+    }
+}
+
+
+treenode* makecopy( const treenode* n ) 
+{
+    if (n == nullptr) {
+        return nullptr;
+    }
+
+    return new treenode(n->val, makecopy(n->left), makecopy(n->right));
+}
+
+
+// Step to the right as long as possible. When we get
+// stuck, we are in the rightmost node. Remove it, and
+// return it. (Don't delete it.)
+// *from cannot be the empty tree.
+
+treenode* extractrightmost( treenode** from )
+{
+   while( (*from) -> right )
+      from = & (*from) -> right;
+
+   treenode* n = *from;
+   *from = (*from) -> left;
+   return n;
+}
+
+void print( std::ostream& out, const treenode* n, unsigned int indent )
+{
+
+   for( unsigned int i = 0; i != indent; ++ i )
+      out << "|  "; 
+   if(n)
+   {
+      out << ( n -> val ) << "\n";
+      print( out, n -> left, indent + 1 );
+      print( out, n -> right, indent + 1 ); 
+   }
+   else
+      out << "#\n"; 
+}
+
+
+// Both the upperbound and the lowerbound are strict.
+// We use pointers, so that they can be absent. 
+// Of course, this is a tasteless way of dealing with partiality.
+
+void checkbetween( const treenode* n, 
+                   const treenode::valtype* lowerbound, 
+                   const treenode::valtype* upperbound ) 
+{
+   treenode::valcmp cmp;
+
+   while(n) 
+   {
+      if( lowerbound && cmp( *lowerbound, n -> val ) >= 0 ) 
+      {
+         std::cout << "value " << ( n -> val );
+         std::cout << " is not above lower bound " << *lowerbound << "\n";
+         throw std::runtime_error( "tree not correct" ); 
+      }
+
+      if( upperbound && cmp( *upperbound, n -> val ) <= 0 ) 
+      {
+         std::cout << "value " << ( n -> val );
+         std::cout << "is not below upperbound " << *upperbound << "\n";
+         throw std::runtime_error( "tree not correct" );  
+      }
+
+      checkbetween( n -> left, lowerbound, &( n -> val ));
+      lowerbound = & ( n -> val );
+      n = n -> right;
+   }
+} 
+
+
+unsigned int log_base2( size_t s )
+{
+    if (s == 0) return 0;
+
+    unsigned int log = 0;
+    while (s > 1) {
+        s >>= 1;
+        ++log;
+    }
+    return log;
+}
+
+bool set::contains( const valtype& val ) const
+{
+    const treenode* result = find(tr, val);
+    return result != nullptr;
+}
+
+ 
+bool set::insert( const valtype& val )
+{
+    treenode** current = find(&tr, val);
+    if (*current == nullptr) {
+        *current = new treenode(val);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+bool set::remove( const valtype& val )
+{
+    treenode** target = find(&tr, val);
+    if (*target == nullptr) {
+        return false;
+    }
+
+    treenode* toRemove = *target;
+
+    if (toRemove->left == nullptr || toRemove->right == nullptr) {
+        treenode* child = (toRemove->left != nullptr) ? toRemove->left : toRemove->right;
+        *target = child;
+        delete toRemove;
+    } else {
+        treenode** pred = find(&toRemove->left, extractrightmost(&toRemove->left)->val);
+        toRemove->val = (*pred)->val;
+        treenode* temp = *pred;
+        *pred = (*pred)->left;
+        delete temp;
+    }
+
+    return true;
+}
+
+
+size_t set::size( ) const
+{
+   return ::size( tr );
+}
+
+size_t set::height( ) const
+{
+   return ::height( tr );
+}
+
+bool set::empty( ) const
+{
+    return tr == nullptr;
+}
+
+set:: ~set( )
+{
+    ::deallocate(tr);
+    tr = nullptr;
+}
+
+void set::clear( )
+{
+    ::deallocate(tr);
+    tr = nullptr;
+}
+
+set::set( const set& other ) :
+    tr(nullptr)
+{
+    tr = makecopy(other.tr);
+}
+
+
+set& set::operator = ( const set& other )
+{
+    if (this != &other) {
+        ::deallocate(tr);
+        tr = ::makecopy(other.tr);
+    }
+
+    return *this;
+}
+
+
+
+void set::checksorted( ) const
+{
+   ::checkbetween( tr, nullptr, nullptr );
+}
+
+
+void set::print( std::ostream& out, unsigned int indent ) const
+{
+   ::print( out, tr, indent );
+}
+
